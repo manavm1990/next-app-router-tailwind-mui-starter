970fb133c4cd47c03844a687428b0b2e5ef1cda0
Manav Misra <manavm@visionwebsoft.com>
2023-05-14 11:16:20 -0500

    chore: :wrench: integrate cypress-vite

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,10 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
+import vitePreprocessor from 'cypress-vite';
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    setupNodeEvents(on) {
+      on('file:preprocessor', vitePreprocessor());
     },
   },
 });
