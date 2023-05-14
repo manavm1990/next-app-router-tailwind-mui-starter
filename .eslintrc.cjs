ed9aa5f2bba5bc839c26c6424978c254fa4a417e
Manav Misra <manavm@visionwebsoft.com>
2023-05-14 10:41:50 -0500

    chore: set up ESLint with Prettier
    Doesn't include testing ✅.
    Includes lint-staged.

---
+++ .eslintrc.cjs
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard-with-typescript',
+    'next/core-web-vitals',
+    'prettier',
+  ],
+  overrides: [
+    {
+      files: ['./src/**/*.tsx'],
+      rules: { '@typescript-eslint/explicit-function-return-type': 1 },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    project: './tsconfig.json',
+    sourceType: 'module',
+  },
+  plugins: ['use-encapsulation'],
+  rules: {
+    'no-console': [
+      'warn',
+      {
+        allow: ['error', 'info', 'table', 'warn'],
+      },
+    ],
+    'no-unused-vars': ['error', { destructuredArrayIgnorePattern: '^_' }],
+    'use-encapsulation/prefer-custom-hooks': ['warn', { allow: ['useMemo'] }],
+  },
+};
