eef3cbc2988fe44c4d8e109cf0ebf0130d85cabc
Manav Misra <manavmisra@planaday.com>
2023-06-02 16:59:02 -0500

    refactor: :fire: remove `Container`

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,21 +1,18 @@
 'use client';
 
 import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
 
 export default function NotFound(): JSX.Element {
   return (
-    <Container>
-      <Box
-        sx={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          height: '100vh',
-        }}
-      >
-        <h1>404 - Page Not Found</h1>
-      </Box>
-    </Container>
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+      }}
+    >
+      <h1>404 - Page Not Found</h1>
+    </Box>
   );
 }
