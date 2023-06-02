4167df4acfb0e0968e2f3522e7c420963d94677e
Manav Misra <manavmisra@planaday.com>
2023-06-02 14:16:30 -0500

    feat: add additional starter files
    Includes docs 📝 update.

---
+++ src/app/loading.tsx
@@ -0,0 +1,12 @@
+// https://nextjs.org/docs/app/api-reference/file-conventions/loading
+
+import Box from '@mui/material/Box';
+import CircularProgress from '@mui/material/CircularProgress';
+
+export default function CircularIndeterminate(): JSX.Element {
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <CircularProgress />
+    </Box>
+  );
+}
