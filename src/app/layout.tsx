8f43f3a34ebbf556044b5e75396a0f3e836588c6
Manav Misra <manavmisra@planaday.com>
2023-06-02 18:49:43 -0500

    fix: resolve runtime typing error for '404.'

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import { Inter } from 'next/font/google';
+import React from 'react';
 import './globals.css';
+import NotFound from './not-found';
 import Theme from './theme';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -16,10 +18,17 @@ export default function RootLayout({
 }): JSX.Element {
   return (
     <html lang="en">
-      <Theme>
-        {' '}
-        <body className={inter.className}>{children}</body>
-      </Theme>
+      <body className={inter.className}>
+        <Theme>
+          {/* 
+            As Next is managing this `RootLayout` and we are not using this 1️⃣, 
+            it's 🆒 to do like this 1️⃣ because Next will ensure that `children` should always be a valid React element. 
+          
+            We are not going to use this ourselves to render other stuff like strings, etc. 
+          */}
+          {React.isValidElement(children) ? children : <NotFound />}
+        </Theme>
+      </body>
     </html>
   );
 }
