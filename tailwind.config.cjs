fd3bc0394ad72ff93ddd966e65716f426fefd439
Manav Misra <manavmisra@planaday.com>
2023-06-03 17:29:02 -0500

    refactor: :building_construction: get rid of 'src'

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,17 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/components/**/*.{ts,jsx,tsx}',
-    './src/app/**/*.{ts,jsx,tsx}',
-  ],
+  content: ['./app/**/*.{ts,jsx,tsx}'],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
+    extend: {},
   },
   plugins: [],
   corePlugins: {
