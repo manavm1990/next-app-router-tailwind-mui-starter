5681f83e79b5dd12d6eecbd5cfc03d4e5fc33898
Manav Misra <manavm@visionwebsoft.com>
2023-06-12 22:33:35 -0500

    chore: :wrench: enable experimental 💩

--- next.config.cjs
+++ next.config.cjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
+    // https://nextjs.org/docs/app/building-your-application/data-fetching/server-actions
+    serverActions: true,
     typedRoutes: true,
   },
 };
